7cfffb31055dcef47f1570d7d12435ff360f5e17
LB <123949568+L1277471578@users.noreply.github.com>
2026-04-03 18:06:13 +0800

    Delete part_segmentation directory

--- part_segmentation/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .build_fn import build_model_from_cfg
-# import models.Mamba3D
-# import models.Mamba3D2
-# import models.Mamba3D3
--- part_segmentation/models/bimamba_ssm/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-import  sys
-sys.path.append('../../models/bimamba_ssm/utils')
